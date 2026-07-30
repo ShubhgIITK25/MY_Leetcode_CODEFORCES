5508b94d80529d20d72bf125f87c688179055611
SHUBH GUPTA <shubhgupta.sg25@gmail.com>
2026-07-30 22:01:35 +0530

    Time: 0 ms (100%), Space: 29.7 MB (46.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,6 +6,22 @@ public:
         reverse(nums.begin(), nums.begin()+n-k);
         reverse(nums.begin()+n-k, nums.end());
         reverse(nums.begin(), nums.end());
-        
+
     }
 };
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//       int n = nums.size();
+//       k = k % n;
+//       for (int i = 0; i < k; i++){
+//         int temp = nums[n-1];
+//         for(int j = n-1; j>0; j--){
+//             nums[j] = nums[j-1];
+//         }
+//         nums[0] = temp;
+//       }
+//     }
+// };
+
