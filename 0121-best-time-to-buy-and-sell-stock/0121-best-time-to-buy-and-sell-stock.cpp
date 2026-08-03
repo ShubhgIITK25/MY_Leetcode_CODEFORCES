0301069a8446b09c90dacc37938459c01122ac6a
SHUBH GUPTA <shubhgupta.sg25@gmail.com>
2026-08-03 17:25:57 +0530

    Time: 2 ms (31.09%), Space: 97.3 MB (61.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int minb =  prices[0];
         int maxp = 0;
-        int mini = prices[0];
         for(int i = 0; i < prices.size(); i++){
-            maxp = max(maxp, prices[i]-mini);
-            mini = min(mini, prices[i]);
+            maxp = max(maxp, prices[i]-minb);
+            minb = min(minb, prices[i]);
         }
         return maxp;
     }
