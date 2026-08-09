4b93fd4ac6aa39cfa4e72b6ab08dfb78a5674cef
SHUBH GUPTA <shubhgupta.sg25@gmail.com>
2026-08-09 12:02:15 +0530

    Add Marenol (easy version) [C1] from Codeforces

---
+++ 2254/C1 - Marenol (easy version)/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        string a, b;
+        cin >> a >> b;
+ 
+        int evenA = 0, oddA = 0;
+        int evenB = 0, oddB = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            if (a[i] == '1') {
+                if (i % 2 == 0)
+                evenA++;
+                else
+                oddA++;
+            }
+ 
+        if (b[i] == '1') {
+            if (i % 2 == 0)
+            evenB++;
+            else
+            oddB++;
+        }
+}
+ 
+if (evenA == evenB && oddA == oddB)
+cout << "YES
+";
+else
+cout << "NO
+";
+}
+ 
+return 0;
+}
