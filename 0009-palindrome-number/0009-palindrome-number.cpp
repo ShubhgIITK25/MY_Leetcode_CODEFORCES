59bc881a093d8e84635afa6513af62ca5522db22
SHUBH GUPTA <shubhgupta.sg25@gmail.com>
2026-09-25 00:13:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long n = 0;
-        long a = x;
-        while(x>0){
-            if(n>INT_MAX) return -1;
-            n = n*10 + x%10;
-            x = x/10;
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int n = 0;
+
+        while (x > n) {
+            n = n * 10 + x % 10;
+            x /= 10;
         }
-        return(n==a)? true:false;
+
+        return x == n || x == n / 10;
     }
 };
